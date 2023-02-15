cf43ec85a4e83dcbdf76809a7c5221ab2a9baa58
Ricardo <stigma746@gmail.com>
2023-02-15 01:11:34 -0300

    Fundamentos básicos. Print y Println para salto de linea

--- app/src/main/java/com/example/fundamentoskotlin/Principal.kt
+++ app/src/main/java/com/example/fundamentoskotlin/Principal.kt
@@ -1,5 +1,14 @@
 package com.example.fundamentoskotlin
 
 fun main(){
-    print("Hola Kotlin")
+    //print("Hola Kotlin")
+    newTopic("Hola Kotlin")
+}
+fun newTopic(topic: String){
+    println()
+    print("==================")
+    print(topic)
+    print("==================")
+
+    print("\n================== $topic ==================\n")
 }
