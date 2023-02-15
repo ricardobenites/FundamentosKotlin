cf2079014827c3ddd1ae2da8852f636846aee5e8
Ricardo <stigma746@gmail.com>
2023-02-15 16:08:09 -0300

    Constantes y variables

--- app/src/main/java/com/example/fundamentoskotlin/Principal.kt
+++ app/src/main/java/com/example/fundamentoskotlin/Principal.kt
@@ -3,6 +3,17 @@ package com.example.fundamentoskotlin
 fun main(){
     //print("Hola Kotlin")
     newTopic("Hola Kotlin")
+
+    newTopic("Variables y Constantes")
+    //val a = 2
+    val a = true
+    println("a = $a")
+
+    //val a = 2
+    var b: Int
+    b = 5
+    println("b = $b")
+
 }
 fun newTopic(topic: String){
     println()
